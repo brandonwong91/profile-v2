b3d24647701eab503bbc3fe496f635b2c57e73e5
Brandon Wong <brandonwongck@gmail.com>
2023-08-29 00:36:12 +0800

    add spotlight

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,29 @@
-import Image from 'next/image'
+"use client"
+import { useState } from "react";
 
 export default function Home() {
+  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+
+  const handleMouseMove = (event: React.MouseEvent) => {
+    setMousePosition({ x: event.clientX, y: event.clientY });
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 animate-dot-scrolling bg-gradient-radial w-full">
-     Hi, Brandon here.
+    <main className="flex min-h-screen relative w-screen p-24" onMouseMove={handleMouseMove}>
+      <div className="pointer-events-none fixed inset-0 z-30 transition duration-300 lg:absolute">
+        <div
+          className="spotlight"
+          style={{
+            background: `radial-gradient(600px at ${mousePosition.x}px ${mousePosition.y}px, rgba(29, 78, 216, 0.15), transparent 80%)`,
+            top: '50%',
+            left: '50%',
+            width: '100%',
+            height: '100%',
+            transform: 'translate(-50%, -50%)',
+          }}
+        />
+      </div>
+      <div className="w-full text-center">Hi, I am Brandon.</div>
     </main>
   )
 }
